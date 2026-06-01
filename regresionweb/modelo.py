6fa9cc927ca9b6d2ad835fb507ad1741ba0df9cf
Hugo <rhizp4060@gmail.com>
2026-06-01 07:50:19 -0700

    Delete regresionweb directory

--- regresionweb/modelo.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-datos = pd.read_csv('datos.csv')
-X = datos[['publicidad']]
-y = datos['ventas']
-modelo = LinearRegression()
-modelo.fit(X, y)
-prediccion = modelo.predict([[120]])
-print(prediccion)
